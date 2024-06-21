17460f97af7dd36eb6e5bf3208ec93e57ba6b80f
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-06-21 12:05:24 +0100

    move page routes into children property of /

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,27 +1,29 @@
-import App from './App.jsx';
-import ErrorPage from './ErrorPage.jsx';
-import HomePage from './home/HomePage.jsx';
-import ShopPage from './ShopPage.jsx';
-import CartPage from './CartPage.jsx'
+import App from "./App.jsx";
+import ErrorPage from "./ErrorPage.jsx";
+import HomePage from "./home/HomePage.jsx";
+import ShopPage from "./ShopPage.jsx";
+import CartPage from "./CartPage.jsx";
 
 const routesConfig = [
-    {
-      path: "/",
-      element: <App />,
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/home",
-      element: <HomePage />,
-    },
-    {
-      path: "/shop",
-      element: <ShopPage />,
-    },
-    {
-      path: "/cart",
-      element: <CartPage />,
-    },
-  ];
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/home",
+        element: <HomePage />,
+      },
+      {
+        path: "/shop",
+        element: <ShopPage />,
+      },
+      {
+        path: "/cart",
+        element: <CartPage />,
+      },
+    ],
+  },
+];
 
-  export default routesConfig;
+export default routesConfig;
