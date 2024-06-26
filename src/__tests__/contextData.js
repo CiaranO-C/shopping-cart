3317104f74fe63bfda00527957c04a9f811112db
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-06-26 11:05:19 +0100

    place mock context data into separate file

---
+++ src/__tests__/contextData.js
@@ -0,0 +1,58 @@
+import { vi } from 'vitest';
+
+const contextData = {
+    plantsData: [
+      {
+        icon: "🏵️",
+        name: "one",
+        id: "1",
+        price: 10,
+      },
+      {
+        icon: "🏵️",
+        name: "two",
+        id: "2",
+        price: 10,
+      },
+      {
+        icon: "🏵️",
+        name: "three",
+        id: "3",
+        price: 10,
+      },
+      {
+        icon: "🏵️",
+        name: "four",
+        id: "4",
+        price: 10,
+      },
+    ],
+    basketData: {
+      1: {
+        icon: "🏵️",
+        name: "one",
+        price: 10,
+        quantity: 1,
+      },
+      2: {
+        icon: "🏵️",
+        name: "two",
+        price: 10,
+        quantity: 2,
+      },
+      3: {
+        icon: "🏵️",
+        name: "three",
+        price: 12,
+        quantity: 3,
+      },
+      4: {
+        icon: "🏵️",
+        name: "four",
+        price: 14,
+      },
+    },
+    setBasketData: vi.fn(),
+  };
+
+  export default contextData;
