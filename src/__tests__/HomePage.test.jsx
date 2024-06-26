6be83f89999bf3c9c2bb04a83596b6f18d709f57
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-06-26 11:04:22 +0100

    remove unused variables

--- src/__tests__/HomePage.test.jsx
+++ src/__tests__/HomePage.test.jsx
@@ -1,65 +1,12 @@
-import { describe, expect, it, vi } from "vitest";
+import { describe, expect, it } from "vitest";
 import HomePage from "../home/HomePage.jsx";
 import { render, screen } from "@testing-library/react";
 import RenderRouteWithOutletContext from "./RenderWithOutletContext.jsx";
 import userEvent from "@testing-library/user-event";
 import { MemoryRouter, Outlet, Route, Routes } from "react-router-dom";
 import ShopPage from "../ShopPage.jsx";
+import contextData from './contextData.js';
 
-const contextData = {
-  plantsData: [
-    {
-      icon: "🏵️",
-      name: "one",
-      id: "1",
-      price: 10,
-    },
-    {
-      icon: "🏵️",
-      name: "two",
-      id: "2",
-      price: 10,
-    },
-    {
-      icon: "🏵️",
-      name: "three",
-      id: "3",
-      price: 10,
-    },
-    {
-      icon: "🏵️",
-      name: "four",
-      id: "4",
-      price: 10,
-    },
-  ],
-  basketData: {
-    1: {
-      icon: "🏵️",
-      name: "one",
-      price: 10,
-      quantity: 1,
-    },
-    2: {
-      icon: "🏵️",
-      name: "two",
-      price: 10,
-      quantity: 2,
-    },
-    3: {
-      icon: "🏵️",
-      name: "three",
-      price: 12,
-      quantity: 3,
-    },
-    4: {
-      icon: "🏵️",
-      name: "four",
-      price: 14,
-    },
-  },
-  setBasketData: vi.fn(),
-};
 
 describe("HomePage component", () => {
   it('Renders with the title "Root" visible on the page', () => {
